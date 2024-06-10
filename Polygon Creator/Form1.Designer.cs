4f696486a13778b58d18c91f677f510f48369ea4
V! <125708505+Vitor-Lucas@users.noreply.github.com>
2024-06-09 23:31:59 -0300

    Start

---
+++ Polygon Creator/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace Polygon_Creator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.resetButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // resetButton
+            // 
+            this.resetButton.Location = new System.Drawing.Point(2, 12);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(81, 41);
+            this.resetButton.TabIndex = 0;
+            this.resetButton.Text = "Reset";
+            this.resetButton.UseVisualStyleBackColor = true;
+            this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.resetButton);
+            this.Name = "Form1";
+            this.Text = "Polygon Creator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button resetButton;
+    }
+}
+
---
+++ Polygon Creator/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Polygon_Creator
+{
+    public partial class Form1 : Form
+    {
+        Graphics g;
+        int click_count = 1;
+
+        List<Line> lines;
+        List<Point> points;
+        Pen pen;
+
+        private void resetButton_Click(object sender, EventArgs e)
+        {
+            g.Clear(Form1.DefaultBackColor);
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(click_count != 1)
+            {
+                Point p = new Point(e.X, e.Y);
+                Line l = new Line(points[click_count-2], p);
+                g.Clear(Form1.DefaultBackColor);
+                l.Draw(g, pen);
+                DrawLines();
+            }
+        }
+
+        private void DrawLines()
+        {
+            foreach(Line l in lines)
+            {
+                l.Draw(g, pen);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            g = CreateGraphics();
+            pen = new Pen(Color.FromArgb(0,0,0));
+            lines = new List<Line>();
+            points = new List<Point>();
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            Point p = new Point(e.X, e.Y);
+            points.Add(p);
+            if (click_count % 2 == 0)
+            {
+                Line l = new Line(points[click_count-1], points[click_count-2]);
+                lines.Add(l);
+                l.Draw(g, pen);
+            }
+            click_count++;
+        }
+    
+    }
+
+    public class Line
+    {
+        public Point p1, p2;
+        public Line(Point p1, Point p2)
+        {
+            this.p1 = p1;
+            this.p2 = p2;
+        }
+
+        public void Draw(Graphics g, Pen pen)
+        {
+            g.DrawLine(pen, this.p1.X, this.p1.Y, this.p2.X, this.p2.Y);
+        }
+    }
+}
